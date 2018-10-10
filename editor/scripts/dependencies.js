1a7fbffdaedcfa50e487127bc97e6afb42b4894e
mkellsy <mkellsy@gmail.com>
2018-10-10 15:17:34 -0600

    rename linter

--- editor/scripts/dependencies.js
+++ editor/scripts/dependencies.js
@@ -3,13 +3,13 @@
 const fs = require("fs-extra");
 
 fs.unlink("./src/monaco/index.vue", () => {
-    fs.copy("../eslint/dist/index.vue", "./src/monaco/index.vue");
+    fs.copy("../linter/dist/index.vue", "./src/monaco/index.vue");
 });
 
 fs.unlink("./src/monaco/monaco.css", () => {
-    fs.copy("../eslint/dist/monaco.css", "./src/monaco/monaco.css");
+    fs.copy("../linter/dist/monaco.css", "./src/monaco/monaco.css");
 });
 
 fs.unlink("./src/monaco/monaco.js", () => {
-    fs.copy("../eslint/dist/monaco.js", "./src/monaco/monaco.js");
+    fs.copy("../linter/dist/monaco.js", "./src/monaco/monaco.js");
 });
